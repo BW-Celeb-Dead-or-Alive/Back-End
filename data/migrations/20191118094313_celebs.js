540ee956506bbfecd1f59f90e5a37c6783ee7375
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-11-18 09:49:21 -0800

    made tables and separate components for scores and celebs

---
+++ data/migrations/20191118094313_celebs.js
@@ -0,0 +1,22 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("celebs", celebs => {
+    celebs.increments();
+
+    celebs
+      .string("name", 255)
+      .notNullable()
+      .unique();
+
+    celebs.string("imageurl", 255).notNullable();
+
+    celebs
+      .boolean("dead")
+      .notNullable()
+      .defaultTo(0)
+      .toString();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("celebs");
+};
