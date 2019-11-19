197855c76a911853825d262d88d22ec0f61e0a8d
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-11-19 10:50:06 -0800

    Seeded celebs

--- data/seeds/001_users.js
+++ data/seeds/001_users.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex("users")
@@ -5,9 +7,24 @@ exports.seed = function(knex) {
     .then(function() {
       // Inserts seed entries
       return knex("users").insert([
-        { id: 1, username: "Forrest", password: "123" },
-        { id: 2, username: "Raj", password: "123" },
-        { id: 3, username: "David", password: "123" }
+        {
+          id: 1,
+          username: "Forrest",
+          password: bcrypt.hashSync("123"),
+          scores: 7 / 10
+        },
+        {
+          id: 2,
+          username: "Raj",
+          password: bcrypt.hashSync("123"),
+          scores: 9 / 10
+        },
+        {
+          id: 3,
+          username: "David",
+          password: bcrypt.hashSync("123"),
+          scores: 10 / 10
+        }
       ]);
     });
 };
